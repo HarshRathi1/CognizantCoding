8dabbb420e14c8cbaffe67e0abae386e0e7d1fcc
HarshRathi1 <harsh.rathi20001@gmail.com>
2022-02-13 22:39:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+pizza=int(input("Enter the number of pizzas bought"))
+puffs=int(input("Enter the number of puffs bought"))
+cd=int(input("Enter the number of cool drinks bought"))
+total=0
+a,b,c=pizza*100,puffs*20,cd*10
+total=a+b+c
+print("Bill Details")
+print("Number of pizzas:",pizza)
+print("Number of puffs:",puffs)
+print("Number of cool drinks:",cd)
+print("Total=",total)
+print("ENJOY THE SHOW!!!")
